8f4d6ce4733727425d0997e2fa8c99e260e63e80
OneEyedWilly293clear <danielinkmou@gmail.com>
2025-12-18 19:27:39 +0200

    Implemented extenstion checker for parsing

---
+++ srcs/parsing/parse.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgueon <jgueon@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 18:19:14 by jgueon            #+#    #+#             */
+/*   Updated: 2025/12/18 19:26:40 by jgueon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ *  @brief calculates the length of a string
+ *
+ * This function counts the number of characters in a string until
+ * it reaches the null terminator '\0'.
+ *
+ * @param s: The string whose length is to be calculated
+ *
+ * @return The number of characters in
+ *	the string (excluding the null terminator)
+ */
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+static void	ft_putstr_err(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(2, &s[i], 1);
+		i++;
+	}
+}
+
+static int	ft_error(const char *msg)
+{
+	ft_putstr_err("Error\n");
+	ft_putstr_err(msg);
+	return (1);
+}
+
+static int	check_extension(const char *file)
+{
+	int	len;
+	int	ext_len;
+
+	len = ft_strlen(file);
+	ext_len = ft_strlen(MAP_EXTENSION);
+	if (len < ext_len)
+		return (0);
+	if (file[len - 4] != MAP_EXTENSION[0])
+		return (0);
+	if (file[len - 3] != MAP_EXTENSION[1])
+		return (0);
+	if (file[len - 2] != MAP_EXTENSION[2])
+		return (0);
+	if (file[len - 1] != MAP_EXTENSION[3])
+		return (0);
+	return (1);
+}
+
+int check_args(int argc, char **argv)
+{
+	int fd;
+
+	if (argc < 2)
+		return(ft_error("No map file provide\n"));
+	if (argc > 2)
+		return(ft_error("Too many arguments provided\n"));
+	if (!check_extension(argv[1]))
+		return (ft_error(FILE_EXT_MSG));
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return (ft_error("Cannot open map file"));
+	close(fd);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	if (check_args(argc, argv) == 0)
+		write(1, "OK: .cub file provided\n", 23);
+	return (0);
+}
---
+++ srcs/parsing/parse.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgueon <jgueon@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 18:35:20 by jgueon            #+#    #+#             */
+/*   Updated: 2025/12/18 19:26:34 by jgueon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_H
+# define PARSE_H
+
+// include <cub3d.h>
+
+# define MAP_EXTENSION ".cub"
+# define FILE_EXT_MSG "Invalid file extension: Use '*.cub' file\n"
+
+#endif
