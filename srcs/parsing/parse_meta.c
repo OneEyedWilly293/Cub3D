a96635267def178970fc3facf88584b7295a4d63
OneEyedWilly293clear <danielinkmou@gmail.com>
2025-12-30 17:05:12 +0200

    further implementation

--- srcs/parsing/parse_meta.c
+++ srcs/parsing/parse_meta.c
@@ -6,7 +6,7 @@
 /*   By: jgueon <jgueon@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 13:40:25 by jgueon            #+#    #+#             */
-/*   Updated: 2025/12/30 13:57:00 by jgueon           ###   ########.fr       */
+/*   Updated: 2025/12/30 16:17:59 by jgueon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,32 @@ static int	is_map_charset(char c)
 */
 static int	is_map_line(char *line)
 {
+	int	i;
 
+	if (!line || line[0] == '\0')
+		return (0);
+	i = 0;
+	while (line[i])
+	{
+		if (!is_map_charset(line[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/*
+** get_line() returns lines without '\n, so empty line =="".
+** But also treat lines of only spaces
+*/
+static int	is_empty_line(char *line)
+{
+	char	*trim;
+
+	if (!line)
+		return (1);
+	trim = skip_spaces(line);
+	if (*trim == '\0')
+		return (1);
+	return (0);
 }
